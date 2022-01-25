64b46e338d560c84b703ccbe66faaade84982c0f
juanmcolorado <jcolorado743@gmail.com>
2022-01-24 19:42:07 -0600

    actualizado

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+const fs = require('fs')
 
-/**
- * Un script que reciba mi nombre como parametro y que nos salude con colores de arcoiris
- * node saludo.js --nombre=charles
- * - Debe ser un nuevo proyecto de node
- * - A terminar compartir el repositorio de github 
- *  - Que no este versionada la caarpeta node_modules
- *  - Que corra el proyecto
- */
+fs.readFile('text.txt', 'utf8', (err, data)=>{
+    if(err){
+        console.error(err)
+    }
+    console.log(data)
+})
