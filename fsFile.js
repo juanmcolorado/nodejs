71eda463c72a66f27c5b8e93755afddd5aa50eb6
juanmcolorado <jcolorado743@gmail.com>
2022-01-26 18:46:47 -0600

    tarea

--- fsFile.js
+++ fsFile.js
@@ -1,4 +1,5 @@
-//write
+/**
+ * //write
 
 const fs = require('fs')
 
@@ -57,3 +58,91 @@ function erase(err){
 }
 
 fs.unlink('Hola.txt', erase)
+*/
+
+const fs = require('fs')
+/**
+ * 1- crear un archivo
+ * 2- agregar una segunda linea
+ * 3- despues de 5 segundos se borra el archivo
+ 
+const filePath = 'voy-a-morir-js'
+
+// paso 1
+fs.writeFile(filePath, 'Hola! no me queda mucho tiempo', 'utf8', (error) => {
+  if(error) {
+    console.error('No se pudo crear el archivo')
+    return
+  }
+
+  // paso 2
+  fs.appendFile(filePath, 'Tengo algo importante que decirte...', (error) => {
+    if (error) {
+      console.error('No se pudo Agregar al archivo')
+      return
+    }
+
+    // paso 3
+    setTimeout(() => {
+      fs.unlink(filePath, (error) => {
+        if (error) {
+          console.error('No se pudo borrar');
+          return
+        }
+        console.log('Adios popo')
+      })
+    }, 5000)
+
+  })
+
+})
+*/
+
+
+const filePath = 'voy-a-morir-js-promises'
+
+// paso 1
+fs.writeFile(filePath, 'Hola! no me queda mucho tiempo', 'utf8', (error) => {
+  if(error) {
+    console.error('No se pudo crear el archivo')
+    return
+  }
+
+  import { writeFile } from 'fs/promises';
+import { Buffer } from 'buffer';
+
+try {
+  const controller = new AbortController();
+  const { signal } = controller;
+  const data = new Uint8Array(Buffer.from('Hello Koders'));
+  const promise = writeFile('hola.txt', data, { signal });
+
+  // Abort the request before the promise settles.
+  controller.abort();
+
+  await promise;
+} catch (err) {
+  // When a request is aborted - err is an AbortError
+  console.error(err);
+}
+  // paso 2
+  fs.appendFile(filePath, 'Tengo algo importante que decirte...', (error) => {
+    if (error) {
+      console.error('No se pudo Agregar al archivo')
+      return
+    }
+
+    // paso 3
+    setTimeout(() => {
+      fs.unlink(filePath, (error) => {
+        if (error) {
+          console.error('No se pudo borrar');
+          return
+        }
+        console.log('Adios popo')
+      })
+    }, 5000)
+
+  })
+
+})
---
+++ promises.js
@@ -0,0 +1,46 @@
+  /**
+ * Usando funciones asicronas de promesas (fsPromises)
+ * 1. Crear un archivo hola.txt
+ * 2. usar la funcion appendFile para agregar una segunda linea
+ * 3. despues de 5 segundos eliminar el archivo (fs.unlink)
+ */
+
+// const fs = require('fs')
+const fsPromises = require('fs/promises')
+
+const file = 'promises_test.txt'
+
+fsPromises.writeFile(file, 'Prueba de promesas', 'utf8')
+    .then(() => {
+        console.log('¡Documento creado en archivos!')
+        fsPromises.appendFile(file, '\nSegunda linea insertada en promesas', 'utf8')
+            .then(() => {
+                console.log('Se inserto una segunda linea')
+                setTimeout(() => {
+                    fsPromises.unlink(file)
+                        .then(() => {
+                            console.log('¡Documento eliminado de archivos!')
+                        })
+                }, 5000)
+            })
+    })
+    .catch(error => {
+        console.error('Error:', error)
+    })
+
+const file1 = 'promises1.txt'
+async function writeFile () {
+    await fsPromises.writeFile(file1, 'Prueba de promesas', 'utf8')
+    await fsPromises.appendFile(file1, '\nSegunda linea insertada en promesas', 'utf8')
+    await setTimeout(() => {
+        fsPromises.unlink(file1)    
+    }, 5000)
+}
+
+writeFile()
+    .then(() => {
+        setTimeout(() => {
+            console.log('Fin (Async Await)')
+        }, 5000);
+        
+    })
